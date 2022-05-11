de5bb8e0ef13c0e41b48e82fdd26e3d8ad1adea6
Oleg Kertanov <okertanov@gmail.com>
2022-05-11 17:50:00 +0300

    Tune plugin configs to fix 1273061/1283635

--- src/TokensTracker/Trackers/NEP-17/Nep17Tracker.cs
+++ src/TokensTracker/Trackers/NEP-17/Nep17Tracker.cs
@@ -102,7 +102,7 @@ namespace Neo.Plugins.Trackers.NEP_17
             var key = new Nep17BalanceKey(balanceChanged.User, balanceChanged.Asset);
             using ScriptBuilder sb = new();
             sb.EmitDynamicCall(balanceChanged.Asset, "balanceOf", balanceChanged.User);
-            using ApplicationEngine engine = ApplicationEngine.Run(sb.ToArray(), snapshot, settings: _neoSystem.Settings);
+            using ApplicationEngine engine = ApplicationEngine.Run(sb.ToArray(), snapshot, settings: _neoSystem.Settings, gas: 1700_0000);
 
             if (engine.State.HasFlag(VMState.FAULT) || engine.ResultStack.Count == 0)
             {
